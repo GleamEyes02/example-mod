8fe200f7f4bd07968967407e7b0f6c5e9efe3b45
GleamEyes02 <162949084+GleamEyes02@users.noreply.github.com>
2026-05-11 20:36:02 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,82 +8,45 @@ using namespace geode::prelude;
 #include <EGL/egl.h>
 #include <time.h>
 
-static bool s_vsyncDisabled = false;
 static int s_targetFps = 60;
-static int64_t s_lastFrameNs = 0;
 
-static void applyDisableVsync() {
+static void applyFps() {
+    auto director = CCDirector::sharedDirector();
+    if (!director) return;
+    director->setAnimationInterval(1.0 / s_targetFps);
+
     EGLDisplay display = eglGetCurrentDisplay();
     if (display != EGL_NO_DISPLAY) {
         eglSwapInterval(display, 0);
-        s_vsyncDisabled = true;
-    }
-}
-
-static int64_t nowNs() {
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
-    return (int64_t)ts.tv_sec * 1000000000LL + ts.tv_nsec;
-}
-
-static void sleepUntilNextFrame() {
-    if (s_targetFps <= 0) return;
-    int64_t frameNs = 1000000000LL / s_targetFps;
-    int64_t now = nowNs();
-
-    if (s_lastFrameNs == 0) {
-        s_lastFrameNs = now;
-        return;
-    }
-
-    int64_t next = s_lastFrameNs + frameNs;
-    int64_t sleepFor = next - now;
-
-    if (sleepFor > 0 && sleepFor < frameNs * 2) {
-        struct timespec req;
-        req.tv_sec = sleepFor / 1000000000LL;
-        req.tv_nsec = sleepFor % 1000000000LL;
-        nanosleep(&req, nullptr);
-        s_lastFrameNs = next;
-    } else {
-        s_lastFrameNs = nowNs();
     }
 }
 
 class $modify(FPSBypassDirector, CCDirector) {
+    struct Fields {
+        bool applied = false;
+    };
+
     void drawScene() {
-        bool vsyncEnabled = Mod::get()->getSettingValue<bool>("disable-vsync");
-
-        if (vsyncEnabled && !s_vsyncDisabled) {
-            applyDisableVsync();
+        if (!m_fields->applied) {
+            applyFps();
+            m_fields->applied = true;
         }
-
         CCDirector::drawScene();
-
-        if (vsyncEnabled) {
-            sleepUntilNextFrame();
-        }
     }
 };
 
 $on_mod(Loaded) {
     s_targetFps = Mod::get()->getSettingValue<int64_t>("fps");
-    s_vsyncDisabled = false;
 
     listenForSettingChanges<int64_t>("fps", [](int64_t value) {
         s_targetFps = (int)value;
-        s_lastFrameNs = 0;
+        applyFps();
     });
 
     listenForSettingChanges<bool>("disable-vsync", [](bool value) {
-        if (!value) {
-            EGLDisplay display = eglGetCurrentDisplay();
-            if (display != EGL_NO_DISPLAY) {
-                eglSwapInterval(display, 1);
-            }
-            s_vsyncDisabled = false;
-        } else {
-            s_vsyncDisabled = false;
+        EGLDisplay display = eglGetCurrentDisplay();
+        if (display != EGL_NO_DISPLAY) {
+            eglSwapInterval(display, value ? 0 : 1);
         }
     });
 }
